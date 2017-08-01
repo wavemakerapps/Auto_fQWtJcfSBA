d29f89fc5899f1764cd3f5666d1f458d349e37fe
LPBVAlWHmk IwMiTvCTnW <appTest1@wavemaker.com>
2017-08-01 10:01:09 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_fQWtJcfSBA",
+  "homePage" : "Main",
+  "name" : "Auto_fQWtJcfSBA",
+  "platformType" : "DEFAULT",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.0"
+};
